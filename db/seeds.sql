479a28a500d448700e240db8f1927b09a99e89b4
jesseziros <jtziros2020@gmail.com>
2020-11-17 19:38:22 -0500

    Deleted node modules from repo and modified some files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Ranch Burger');
+INSERT INTO burgers (burger_name) VALUES ('Western-style BBQ Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
